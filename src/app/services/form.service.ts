db1e6fc283b2cb600196e0dfb65ba5eddeafd542
Sujatha Manoharan <Sujathamanoharan7@gmail.com>
2025-06-23 20:04:49 -0700

    delete row functionality added

--- src/app/services/form.service.ts
+++ src/app/services/form.service.ts
@@ -47,4 +47,12 @@ export class FormService {
     const rows = this._rows();
     this._rows.set([...rows,newRows])
   }
+  deleteRow(rowId:string){
+    if(this._rows().length == 1){
+      return;
+    }
+    const rows = this._rows();
+    const newRows = rows.filter(row => row.id !== rowId);
+    this._rows.set(newRows)
+  }
 }
